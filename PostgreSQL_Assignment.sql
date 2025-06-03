6e4b724864740a3f595010596f59dae5167989b4
Your Name <you@example.com>
2025-06-03 09:12:39 +0600

    Solved of problem 1,2,3,4

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,48 +1,137 @@
 -- Active: 1748654107880@@127.0.0.1@5432@conservation_db
-CREATE TABLE rangers(
+CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     region VARCHAR(255) NOT NULL
-    );
+);
 
-
-CREATE TABLE species(
+CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(255) NOT NULL,
     scientific_name VARCHAR(255),
     discovery_date DATE,
     conservation_status VARCHAR(15)
-    );
+);
 
-CREATE TABLE sightings(
+CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    ranger_id INTEGER REFERENCES rangers(ranger_id),
-    species_id INTEGER REFERENCES species(species_id),
+    ranger_id INTEGER REFERENCES rangers (ranger_id),
+    species_id INTEGER REFERENCES species (species_id),
     sighting_time TIMESTAMP without time zone,
     location VARCHAR(255),
     notes VARCHAR(255)
+);
+
+INSERT INTO
+    rangers (name, region)
+VALUES (
+        'Alice Green',
+        'Northern Hills'
+    ),
+    ('Bob White', 'River Delta'),
+    (
+        'Carol King',
+        'Mountain Range'
     );
 
-INSERT INTO rangers(name, region)
-VALUES
-    ('Alice Green','Northern Hills'),
-    ('Bob White','River Delta'),
-    ('Carol King','Mountain Range');
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
 
-INSERT INTO species(common_name,scientific_name,discovery_date,conservation_status)
-VALUES
-    ('Snow Leopard','Panthera uncia','1775-01-01','Endangered'),
-    ('Bengal Tiger','Panthera tigris tigris','1758-01-01','Endangered'),
-    ('Red Panda','Ailurus fulgens','1825-01-01','Vulnerable'),
-    ('Asiatic Elephant','Elephas maximus indicus','1758-01-01','Endangered');
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        null
+    );
 
-INSERT INTO sightings(species_id,ranger_id,location,sighting_time,notes)
-VALUES
-    (1,1,'Peak Ridge','2024-05-10 07:45:00','Camera trap image captured'),
-    (2,2,'Bankwood Area','2024-05-12 16:20:00','Juvenile seen'),
-    (3,3,'Bamboo Grove East','2024-05-15 09:10:00','Feeding observed'),
-    (1,2,'Snowfall Pass','2024-05-18 18:30:00',null);
+SELECT * FROM rangers;
 
-    SELECT * FROM rangers;
-    SELECT * FROM species;
-    SELECT * FROM sightings;
+SELECT * FROM species;
+
+SELECT * FROM sightings;
+
+--Problem 1
+--Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+
+INSERT INTO
+    rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+--Problem 2
+--Count unique species ever sighted.
+
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+--Problem 3
+--Find all sightings where the location includes "Pass".
+
+SELECT * FROM sightings
+WHERE location LIKE '%Pass%';
+
+--Problem 4
+--List each ranger's name and their total number of sightings.
+
+SELECT name,COUNT(s.ranger_id) AS total_sightings 
+FROM rangers AS r
+JOIN sightings AS s USING (ranger_id)
+GROUP BY name;
